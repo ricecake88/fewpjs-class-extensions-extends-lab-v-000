9086167bef54ebe9ef195757e74e9b667abebc58
Grace Shih <vocalise@gmail.com>
2020-03-11 05:46:51 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -60,7 +60,6 @@ describe( "Square", () => {
   it( "calculates the area", () => {
     let square = new Square( [ 5, 5, 5, 5 ] )
 
-   console.log(square.area);
     expect( square.area ).to.eq( 25 )
   } )
 
