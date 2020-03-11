ab4b78cc642acd61cce71ec49ff602a68d027913
Grace Shih <vocalise@gmail.com>
2020-03-11 05:18:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Polygon {
 
 class Triangle extends Polygon {
   get isValid() {
-    if ((this.countSides === 3) && (this.sides[0] + this.sides[1] > this.sides[2] || this.sides[1] + this.sides[2] > this.sides[0] || this.sides[2] + this.sides[0] > this.sides[1])) {
+    if ((this.countSides === 3) && ((this.sides[0] + this.sides[1]) > this.sides[2] || (this.sides[1] + this.sides[2]) > this.sides[0] || (this.sides[2] + this.sides[0]) > this.sides[1])) {
             return true;
           }
      else {
