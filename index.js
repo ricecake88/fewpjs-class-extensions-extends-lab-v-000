9b933cdba13a2285c17527427bdb18e2b954ff7d
Grace Shih <vocalise@gmail.com>
2020-03-11 04:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  
+  get countSides() {
+    
+  }
+  
+  get permieter() {
+    
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    
+  }
+}
