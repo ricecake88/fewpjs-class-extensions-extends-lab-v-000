8d73a315aa3ece8dd7f446140265d57f9279383a
Grace Shih <vocalise@gmail.com>
2020-03-11 05:39:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ class Square extends Polygon {
   }
   
   get area() {
+    console.log("blah")
     console.log(this.isValid)
     if (this.isValid === true) {
       return this.side[0]*this.side[1];
